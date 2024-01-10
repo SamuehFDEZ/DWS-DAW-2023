d3be94e09ac4b4fa3fb8ba618f65880e9a83da53
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2024-01-10 17:20:48 +0100

    phpoo agregado

---
+++ EjUD8_POO_Samuel_Arteaga/Incidencia.php
@@ -0,0 +1,69 @@
+<?php 
+
+class Incidencia{
+    static $codigoIncidencia = 0;
+    static $contadorPendientes = 0;
+    private $codigo; 
+    private $numero;
+    private $incidencia;
+    private  $estado = "Pendiente"; 
+    private $solucion;
+
+    public function getCodigoIncidencia(){
+        return self::$codigoIncidencia;
+    }
+
+    public function setCodigoIncidencia($codigoIncidencia){
+        $this->codigoIncidencia = $codigoIncidencia;
+    }
+
+    public function getNumero(){
+        return $this->numero;
+    }
+
+    public function setNumero($numero){
+        $this->$numero = $numero;
+    }
+
+    public function getIncidencia(){
+        return $this->incidencia;
+    }
+
+    public function setIncidencia($incidencia){
+        $this->incidencia = $incidencia;
+    }
+
+    public function getEstado(){
+        return $this->estado;
+    }
+
+    public function setEstado($estado){
+        $this->estado = $estado;
+    }
+
+    public function __construct($numero, $incidencia) {
+        self::$codigoIncidencia++;
+        $this->codigo = self::$codigoIncidencia;
+        $this->numero = $numero;
+        $this->incidencia = $incidencia;
+    }
+
+    public function resuelve($solucion){
+        $this->solucion = $solucion;
+        $this->estado = "Resuelto";
+        self::$codigoIncidencia--;
+    }
+    
+
+    public static function getPendientes(){
+        return self::$codigoIncidencia;
+        
+    }
+
+    public function __toString(){
+        return "Incidencia ". $this->codigo ." - Puesto: $this->numero - $this->incidencia - ". $this->getEstado()." - ". $this->solucion." \n";
+    }
+}
+
+
+?>
